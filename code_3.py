dffc3bc98e2a16c81fb46ecba91679b860abc845
genfilweb <142978168+genfilweb@users.noreply.github.com>
2023-10-20 19:16:51 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,69 @@
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+
+    }
+    const data = [];
+    return data;
+
+main();
+
+}
+}
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    });
+    data.forEach(item => {
+function main() {
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    main()
+def main():
+def generate_random_data():
+if __name__ == "__main__":
+
+
+    for item in data:
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+if __name__ == "__main__":
+    data = generate_random_data()
+
+    for item in data:
+
+import random
+def generate_random_data():
+    main()
+
+        print(f"Random Number: {item}")
+def main():def main():
+
+    main()
+    for item in data:
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+import random
+    data = generate_random_data()
+
+
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]import random
+    return data
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+        print(f"Random Number: {item}")
+    main()
+    data = generate_random_data()
+    for item in data:
+
+
+if __name__ == "__main__":
