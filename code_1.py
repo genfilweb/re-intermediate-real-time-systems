2273fc21a2a9e5fa7f45700618724c86818bb241
genfilweb <142978168+genfilweb@users.noreply.github.com>
2023-10-26 00:57:54 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,17 +1,13 @@
-    });
-    }
 
-}
-    data.forEach(item => {
-function main() {
-    const data = generateRandomData();
-}
-function generateRandomData() {
+        print(f"Random Number: {item}")
+    data = generate_random_data()
 
-    return data;
-        console.log("Random Number: " + item);
-main();
-    for (let i = 0; i < 10; i++) {
+    data = [random.randint(1, 100) for _ in range(10)]
 
-    const data = [];
-        data.push(Math.floor(Math.random() * 100) + 1);
+import random
+def generate_random_data():
+def main():
+    for item in data:
+    main()
+if __name__ == "__main__":
+    return data
