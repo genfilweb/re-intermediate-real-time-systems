51b497111a5d9d630e1bfee42fb0a59f577cffd3
genfilweb <142978168+genfilweb@users.noreply.github.com>
2023-11-03 09:07:05 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,69 +1,26 @@
-
-    main()
-if __name__ == "__main__":
-def generate_random_data():
-
 import random
-def main():
-
-        print(f"Random Number: {item}")
-
     data = [random.randint(1, 100) for _ in range(10)]
-    return data
     data = generate_random_data()
-    for item in data:def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    main()
-def main():
-    return data
-if __name__ == "__main__":
 
     for item in data:
-import random
-
-
-    data = generate_random_data()
-        print(f"Random Number: {item}")
 
 def main():
-    return data
-    data = generate_random_data()
 
+    return data
 if __name__ == "__main__":
         print(f"Random Number: {item}")
-    for item in data:
+
     main()
+def generate_random_data():    main()
+    data = generate_random_data()
+    for item in data:
+def main():
 import random
-
 def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
 
     return data
-    for item in data:
 
-import random
-def main():
         print(f"Random Number: {item}")
-
-def generate_random_data():
-    data = generate_random_data()
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-}
-
-    }
-function generateRandomData() {
-    });
-    return data;
-        console.log("Random Number: " + item);
-    const data = [];
-function main() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-main();
-
-    const data = generateRandomData();
-
-    data.forEach(item => {
-    for (let i = 0; i < 10; i++) {
-}
