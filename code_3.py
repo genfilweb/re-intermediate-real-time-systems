8f106b99e02a8a5724aa83f7db884958b4a9b7b3
genfilweb <142978168+genfilweb@users.noreply.github.com>
2023-11-05 11:22:41 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,29 @@
-def generate_random_data():
-    return data
-import random
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-if __name__ == "__main__":
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+    });
+function generateRandomData() {
+    const data = [];
+    data.forEach(item => {
+    const data = generateRandomData();
+main();
+    return data;
+    for (let i = 0; i < 10; i++) {
+    }
+function main() {
+}
 
     for item in data:
-def main():
-
     data = generate_random_data()
 
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+import random
+
+def main():
 
         print(f"Random Number: {item}")
+    main()
